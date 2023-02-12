b109d1df2d3df9f68f096d1b2c8604defb07722a
holuboski <holuboski@gmail.com>
2023-02-12 16:43:26 -0300

    Meu Primeiro Script

---
+++ Aula01/script.js
@@ -0,0 +1,13 @@
+console.log("Hello World! Olá Mundo!")
+
+{
+    let nome = "Pedro Legal"
+    //console.log(nome)
+}
+console.log(nome)
+
+{
+    var nome2 = "Pedro Legal"
+}
+console.log(nome2)
+
